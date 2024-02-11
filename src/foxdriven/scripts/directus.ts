116806bc32a3685ee27639c793b97cab94f05ac3
wol4aravio <panovskiy.v@yandex.ru>
2024-02-11 20:10:28 +0300

    fixed user + article fetch

--- src/foxdriven/scripts/directus.ts
+++ src/foxdriven/scripts/directus.ts
@@ -1,10 +1,14 @@
+import { atom } from "nanostores";
 import { createDirectus, rest, authentication } from "@directus/sdk";
 
 const cms_url = await import.meta.env.CMS_URL;
 const cms_user = await import.meta.env.CMS_USER;
 const cms_password = await import.meta.env.CMS_PASSWORD;
 
-const client = createDirectus(cms_url).with(rest()).with(authentication());
-await client.login(cms_user, cms_password);
+const client = createDirectus(cms_url)
+  .with(authentication("json"))
+  .with(rest());
+const result = await client.login(cms_user, cms_password);
 
 export const directusClient = client;
+export const $refreshToken = atom(result["refresh_token"]?.toString());
